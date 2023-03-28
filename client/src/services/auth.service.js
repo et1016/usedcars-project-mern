a439a4c134d6d3b4f1b79479eee354b71e7aa0cf
et1016 <sunlightten@gmail.com>
2023-03-28 21:41:19 +0800

    front-end connection back-end url update

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://creepy-puce-apron.cyclic.app/api/user";
 
 class AuthService {
   register(username, email, password) {
